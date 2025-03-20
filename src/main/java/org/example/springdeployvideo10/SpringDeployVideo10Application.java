fdff5d6f78e7db6c2a8302a843ca8b04b99f3203
Fran Santana <santanafran96@gmail.com>
2025-03-20 13:18:28 +0100

    spring heroku (Probablemente no se pueda) app config

---
+++ src/main/java/org/example/springdeployvideo10/SpringDeployVideo10Application.java
@@ -0,0 +1,13 @@
+package org.example.springdeployvideo10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeployVideo10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeployVideo10Application.class, args);
+    }
+
+}
